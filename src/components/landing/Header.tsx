8ee7ed36922fc6e7a4e82353333f63802fe8d5ae
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-04-03 23:24:04 +0530

    add tsx in Header

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    /* header For Mobile */
+    <div className='sticky top-0 left-0 right-0 z-50'>
+        <div className="bg-white dark:bg-black/5 w-full">
+        
+        </div>
+    </div>
+    </>
   )
 }
 
