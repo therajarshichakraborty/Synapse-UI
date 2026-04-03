90e3adbee726c83e14a2acf911e1f257f8c43349
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-04-03 21:28:30 +0530

    add (root)/layout.tsx

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+import { ReactNode } from "react";
+import { Header } from "@/components/landing/header";
+import { Footer } from "@/components/layout/footer";
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Synapse UI - Open Source UI Components for React",
+    default: "Synapse UI - Open Source UI Components for React",
+  },
+};
+
+export default function HomeLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="relative w-full pt-0 md:pt-0 bg-white dark:bg-black">
+        {children}
+      </main>
+      <Footer/>
+    </>
+  );
+}
