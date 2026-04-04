a7cf4920691238629ae9b7473617f4daabe8be9c
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-04-04 11:16:23 +0530

    bug fixes

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { ReactNode } from "react";
-import  Header  from "@/components/landing/Header";
-import  Footer  from "@/components/layout/Footer";
+import Header from "@/components/landing/Header";
+import Footer from "@/components/layout/Footer";
 export const metadata: Metadata = {
   title: {
     template: "%s | Synapse UI - Open Source UI Components for React",
@@ -16,7 +16,7 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
       <main className="relative w-full pt-0 md:pt-0 bg-white dark:bg-black">
         {children}
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
