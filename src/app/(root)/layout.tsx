9938db9ab9744a69e6bd490da22e67f15595b912
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-04-04 17:56:02 +0530

    changed font

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,18 @@ import { Metadata } from "next";
 import React, { ReactNode } from "react";
 import Header from "@/components/landing/Header";
 import Footer from "@/components/layout/Footer";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: {
     template: "%s | Synapse UI - Open Source UI Components for React",
@@ -11,12 +23,12 @@ export const metadata: Metadata = {
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <Header />
       <main className="relative w-full pt-0 md:pt-0 bg-white dark:bg-black">
         {children}
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
